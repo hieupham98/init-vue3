8a83fae586c26d1c4e61466eae557d3702c8ffe0
hieupham98 <127081659+hieupham98@users.noreply.github.com>
2023-10-30 10:02:12 +0700

    Revert "Revert "add konvajs""

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createStore } from 'vuex'
+import VueKonva from 'vue-konva';
 
 import './assets/main.css'
 const store = createStore({
@@ -23,5 +24,6 @@ const app = createApp(App)
 app.use(store)
 
 app.use(router)
+app.use(VueKonva)
 
 app.mount('#app')
